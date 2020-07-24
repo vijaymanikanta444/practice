01122e8c9c49878c3ca9fe974121fb12f7d5d460
vijaymanikanta444 <vijayakp444@gmail.com>
2020-07-24 21:38:21 +0530

    day_1

---
+++ day_1.js
@@ -0,0 +1,39 @@
+// console.log('vijay');
+
+// var name;
+// console.log(name);
+// name = 'vinay';
+// console.log(name);
+
+// var a, b;
+// a = 10;
+// b = 20;
+// console.log(a + b);
+
+// function greeting(name) {
+//   console.log('Hello ' + name);
+// }
+
+// greeting('vijay');
+// greeting('reshma');
+// greeting('paramesh');
+// greeting('harish');
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(1, 2);
+// add(15, 25);
+// add(45, 27);
+
+// function add(a, b) {
+//   return a + b;
+// }
+// var result = add(1, 2);
+// console.log(result);
+// // console.log(add(1, 2));
+
+function sum(a, b) {
+  console.log(a + b);
+}
+console.log(sum(1, 2));
